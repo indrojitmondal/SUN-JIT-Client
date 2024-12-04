2ecce278fe948374bb12f58a58c47eb296c753e1
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-04 21:51:59 +0600

    Added Guidlines Section

---
+++ src/components/ExploreGame.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ExploreGame = () => {
+    return (
+        <div>
+              <section  className="my-10 w-11/12 lg:w-10/12 mx-auto">
+                <h1 className="font-bold text-center text-p2 mt-1 md:text-xl">GUIDELINES TO PLAY GAME</h1>
+               
+
+                <p>Hello...from GuideLine</p>
+              </section>
+        </div>
+    );
+};
+
+export default ExploreGame;
--- src/components/layoutComponent/MainLayout.jsx
+++ src/components/layoutComponent/MainLayout.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Sliders from '../Sliders';
 import Contact from '../Contact';
 import HighestRatedGame from '../HighestRatedGame';
+import ExploreGame from '../ExploreGame';
 
 const MainLayout = () => {
     return (
         <div>
               <Sliders></Sliders>
               <HighestRatedGame></HighestRatedGame>
+              <ExploreGame></ExploreGame>
               <Contact></Contact>
         </div>
     );
