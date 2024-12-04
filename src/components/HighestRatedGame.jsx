aa3e7eda53bfd488a754501b2289724cc8974775
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-04 21:39:46 +0600

    Added Highest Rated Game section

---
+++ src/components/HighestRatedGame.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HighestRatedGame = () => {
+    return (
+        <div>
+             <section  className="my-10 w-11/12 lg:w-10/12 mx-auto">
+                <h1 className="font-bold text-center text-p2 mt-1 md:text-xl">HIGHEST RATED GAME</h1>
+               
+
+                <p>Hello...from HighestRatedGame</p>
+            </section>
+        </div>
+    );
+};
+
+export default HighestRatedGame;
--- src/components/layoutComponent/MainLayout.jsx
+++ src/components/layoutComponent/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sliders from '../Sliders';
 import Contact from '../Contact';
+import HighestRatedGame from '../HighestRatedGame';
 
 const MainLayout = () => {
     return (
         <div>
               <Sliders></Sliders>
+              <HighestRatedGame></HighestRatedGame>
               <Contact></Contact>
         </div>
     );
