7e0e8cfdafff3036f016b03d25d20d0066b6b82c
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-07 17:35:58 +0600

    Update Table and added react starts

--- src/components/GameWatchList.jsx
+++ src/components/GameWatchList.jsx
@@ -6,9 +6,9 @@ const GameWatchList = () => {
     const watchListLoaded = useLoaderData();
     console.log('watchList:',watchListLoaded);
     return (
-        <div>
-            <h2>Hello from GameWatchList, total watchList: {watchListLoaded.length} </h2>
-            
+        <div className='mt-5'>
+            {/* <h2>Hello from GameWatchList, total watchList: {watchListLoaded.length} </h2>
+             */}
             <div className='w-11/12 lg:w-10/12 mx-auto '>
 
                 <table style={{ width: "100%" }}>
--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
+// import StarRatings from './react-star-ratings';
+import ReactStars from 'react-stars'
 
-const TableRow = ({game,index}) => {
-    console.log('Hello from Table row',game);
+const TableRow = ({ game, index }) => {
+    // console.log('Hello from Table row', game);
     return (
-      
-               <tr>
-                            <td>{index+1}</td>
-                            <td><h2>{game.game_title}</h2>
-                                <img src={game.game_url} className='' alt="" />
-                                 <div>{game.rating}</div>
-                            </td>
-                            <td>{game.genres}</td>
-                           <td>{game.game_description}</td>
+
+        <tr>
+            <td className='text-center'>{index + 1}</td>
+            <td><h2>{game.game_title}</h2>
+                <img src={game.game_url} className='' alt="" />
+                {/* <div>{game.rating}</div> */}
+                {/* <StarRatings
+                    rating={game.rating || 0}
+                    starRatedColor="blue"
+                    // changeRating={this.changeRating}
+                    numberOfStars={5}
+                    name='rating'
+                /> */}
+
+                <div className='flex items-center gap-4'>
+                    <ReactStars count={5}
+
+                        size={24}
+                        activeColor="#ffd700"
+                        value={game.rating}
+                    >
+
+                    </ReactStars>
+
+                    {game.rating}
+                </div>
+
+            </td>
+            <td>{game.genres}</td>
+            <td>{game.game_description}</td>
 
 
-                            
-                            <td>{game.publication_year}</td>
 
-               </tr>
-     
+            <td>{game.publication_year}</td>
+
+        </tr>
+
     );
 };
 
