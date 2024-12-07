673c5d459b8c638005ed0f39827084986300ba72
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-07 13:54:31 +0600

    Updated in Addto watchlist

--- src/components/ExploreDetails.jsx
+++ src/components/ExploreDetails.jsx
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { AuthContext } from './providers/AuthProvider';
 
 const ExploreDetails = () => {
     const loadedData = useLoaderData();
+    const {user}=useContext(AuthContext);
     const { game_url, game_title, game_description, rating, publication_year, genres, name, email } = loadedData;
-
+    
     //console.log('Name from Explore Details: ', name);
     //console.log('Data from Explore Details: ', loadedData);
+    
+    const handleAddToWatchList=()=>{
+       // alert('Clicked Add to WatchList');
+        console.log('Post this data to watchList', loadedData);
+        if(user){
+        fetch('https://sunjit-server.vercel.app/myWatchList', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(loadedData)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('Data from API:', data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Added to WatchList',
+                        text: 'Review has been added to watchList successfully ',
+                        icon: 'success',
+                        confirmButtonText: 'OK'
+                    })
+                   // form.reset();
+                }
+            })
+            .catch(error => {
+                console.error("Error adding review:", error);
+                Swal.fire({
+                    title: 'Error',
+                    text: 'The review maybe already added to your watchList or not logged in. Please try again. ',
+                    icon: 'error',
+                    confirmButtonText: 'OK'
+                });
+            })
+            .finally(() => {
+                // Stop loading after request is finished
+                //setLoading(false);
+            });
+        }
+        else{
+            
+            Swal.fire({
+                title: 'Error',
+                text: 'You are not able to add this review to watchList. Please log in and try again. ',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            });
+        }
+    
+    }
+
     return (
         <div className='w-11/12 lg:w-11/12 mx-auto'>
             <h2 className='text-2xl text-center py-4 font-bold'>Review Details</h2>
@@ -25,7 +79,7 @@ const ExploreDetails = () => {
                     </div>
                     <div className='flex justify-center'> 
 
-                    <button className='btn  '>Add to WatchList"</button>
+                    <button onClick={handleAddToWatchList} className='btn  '>Add to WatchList"</button>
                 
 
                     </div>
--- src/components/GameWatchList.jsx
+++ src/components/GameWatchList.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const GameWatchList = () => {
+    const watchListLoaded = useLoaderData();
     return (
         <div>
-            <h2>Hello from GameWatchList</h2>
+            <h2>Hello from GameWatchList, total watchList: {watchListLoaded.length} </h2>
         </div>
     );
 };
