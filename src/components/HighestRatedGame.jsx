064f4e31e6c7c9937caafbaa83a1b3757dad1904
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-08 00:47:54 +0600

    Loaded high rated data

--- src/components/HighestRatedGame.jsx
+++ src/components/HighestRatedGame.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Fade, Slide } from "react-awesome-reveal";
 const HighestRatedGame = () => {
+    const [highRatedReviews, setHighRatedReviews]= useState([]);
+    useEffect(()=>{
+        fetch('https://sunjit-server.vercel.app/highRatedGames')
+        .then(res=> res.json())
+        .then(data=> setHighRatedReviews(data));
+    },[])
     return (
         <div>
              <section  className="my-10 w-11/12 lg:w-10/12 mx-auto">
           
                 <Slide>
                            
-                            <h1 className="font-bold text-center text-p2 mt-1 text-3xl">HIGHEST RATED GAMES</h1>
+                        <h1 className="font-bold text-center text-p2 mt-1 text-3xl">HIGHEST RATED GAMES</h1>
                
-                        </Slide>
-                <p>Hello...from HighestRatedGame</p>
+                 </Slide>
+                <p>Hello...from HighestRatedGame. High Data length from API: {highRatedReviews.length} </p>
                 
 
 
