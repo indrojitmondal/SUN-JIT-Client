26ee6027ed6c766d53ec3b92cb1ebc4e0a29e467
indroji-mondal <indro.cse.bu@gmail.com>
2024-12-08 21:04:03 +0600

    Update in slides and Here is the End for the Assignment

--- src/components/Sliders.jsx
+++ src/components/Sliders.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import slide1 from '../assets/hero-carousel/first.png';
-import slide2 from '../assets/hero-carousel/1.jpg';
-import slide3 from '../assets/hero-carousel/3.png';
+import slide2 from '../assets/hero-carousel/4.png';
+// import slide2 from '../assets/hero-carousel/1.jpg';
+import slide3 from '../assets/hero-carousel/snakeLudo.png';
 import slide4 from '../assets/hero-carousel/4.jpg';
 
 
